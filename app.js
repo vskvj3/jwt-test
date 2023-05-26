c3c4c180c116c78fa899f0fa43f7ecacc78a2a45
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-05-26 21:43:08 +0530

    import model in into app

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const app = express();
 
 app.use(express.json());
 
-// Logic goes here
+// importing user context
+const User = require("./model/user");
+
+// Register
+app.post("/register", (req, res) => {
+// our register logic goes here...
+});
+
+// Login
+app.post("/login", (req, res) => {
+// our login logic goes here
+});
 
 module.exports = app;
